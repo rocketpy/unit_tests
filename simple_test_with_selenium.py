6e24cbd7c9fe1c164765477d097419a896afb5bd
rocketpy <dimitr.ushakov@gmail.com>
2020-05-08 22:15:04 +0300

    updated

--- simple_test_with_selenium.py
+++ simple_test_with_selenium.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 driver = webdriver.Chrome()
 driver.get("https:// ... ")
 
-search = driver.find_element_by_id("id_name")
+search = driver.find_element_by_id("id_name")  # on web_page searh input
+search.send_keys("some word_key")
+
+search_button = driver.find_element_by_xpath("xpath_of_element")
+search_button.click()
+
+#  for checking , we are on correct web_page or not
+assert "some word_key" in driver.title
+
+driver.quit()
